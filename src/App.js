ac22d608dd429da9b941a19c9b016abad76b182f
Joqueiroz <joaoqueiroz964@gmail.com>
2025-03-24 15:13:32 -0300

    Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Login from "./screens/Login";
 import Cadastro from "./screens/Cadastro";
 import Home from "./screens/Home";
+import Layout from "./components/Layout";
 import CadastroEventoScreen from './screens/CadastroEventoScreen';
 import CadastroOrganizadorScreen from './screens/CadastroOrganizadorScreen';
 import CadastroIngressoScreen from './screens/CadastroIngressoScreen';
-
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 
@@ -14,8 +14,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Cadastro" component={Cadastro}/>
+        <Stack.Screen name="Login" component={()=>(
+          <Layout>
+            <Login/>
+          </Layout>
+        )}/>
+        <Stack.Screen name="Cadastro" component={()=>(
+          <Layout>
+            <Cadastro/>
+          </Layout>
+        )}/>
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="CadastroEventoScreen" component={CadastroEventoScreen} />
         <Stack.Screen name="CadastroOrganizadorScreen" component={CadastroOrganizadorScreen} />
